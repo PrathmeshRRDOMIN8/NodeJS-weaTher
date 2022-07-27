00e0f9479507014ada4c87e3119e449b6541c49f
Prathmesh Sinha <77191932+PrathmeshRRDOMIN8@users.noreply.github.com>
2022-07-27 12:43:54 +0530

    Remove unnecessary console.log commands

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Client side file is loaded!') 
+// console.log('Client side file is loaded!') 
 
 // fetch('http://puzzle.mead.io/puzzle').then((response)=>{
 //      response.json().then((data)=>{
